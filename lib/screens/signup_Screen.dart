4d0a598649f5052af37c59170d71f97c13e97cc0
Sameer Nayak <yoursameer82@gmail.com>
2026-08-01 00:15:34 +0530

    latest commit

--- lib/screens/signup_Screen.dart
+++ lib/screens/signup_Screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
